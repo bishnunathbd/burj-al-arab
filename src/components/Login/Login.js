b37bb6279efa2dd95ac2e87e5af2c5e567699c04
bishnunathbd <bishnunath774@gmail.com>
2021-04-18 12:26:16 +0600

    44-2-get simple google login auth set

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { Button } from '@material-ui/core';
 
 // Initialize Firebase
 if (firebase.apps.length === 0) {
@@ -9,9 +10,29 @@ if (firebase.apps.length === 0) {
 }
 
 const Login = () => {
+  const handleGoogleSignIn = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider)
+      .then((result) => {
+        const { displayName, email } = result.user;
+        const signedInUser = { name: displayName, email };
+        console.log(signedInUser);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+
   return (
-    <div>
-      
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <Button onClick={handleGoogleSignIn} variant='contained' color='primary'>Google sign in</Button>
     </div>
   );
 };
